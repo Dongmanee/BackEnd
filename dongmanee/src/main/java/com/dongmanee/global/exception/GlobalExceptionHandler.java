22493ebbe247d529c7ad4f291a858e9e4ba6c422
seongjun-noh <seongjun5657@gmail.com>
2023-11-15 02:17:34 +0900

    feat: 전역 에러 처리를 위한 GlobalExceptionHandler 추가

---
+++ dongmanee/src/main/java/com/dongmanee/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.dongmanee.global.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.dongmanee.global.utils.ApiResponse;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+	@ExceptionHandler(CustomException.class)
+	public ResponseEntity<ApiResponse> handleDuplicateEmailException(CustomException ex) {
+		return ApiResponse.error(ex.getHttpStatus(), ex.getMessage());
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ApiResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+		return ApiResponse.error(HttpStatus.BAD_REQUEST,
+			ex.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+	}
+}
