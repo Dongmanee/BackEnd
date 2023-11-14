b9cdcd5a575af68f31c631c956492c95e3ba6f61
seongjun-noh <seongjun5657@gmail.com>
2023-11-15 02:33:38 +0900

    feat: 스프링 시큐리티 기본설정, CORS 설정 추가

---
+++ dongmanee/src/main/java/com/dongmanee/global/security/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.dongmanee.global.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.httpBasic(httpSecurityHttpBasicConfigurer -> httpSecurityHttpBasicConfigurer.disable()) // spring security 기본 인증 해제
+			.csrf(AbstractHttpConfigurer::disable) // CSRF 공격 방지 기능 해제
+			.cors(Customizer.withDefaults()) // CORS 설
+			.headers((headerConfig) ->    // h2-console 표시를 위해
+				headerConfig.frameOptions(frameOptionsConfig ->
+					frameOptionsConfig.disable()
+				)
+			)
+			.authorizeHttpRequests(authorizedRequests -> // HTTP 요청에 대한 인가 설정
+				authorizedRequests.anyRequest().permitAll() // 모든 요청에 대해서 인증 없이 허용
+			);
+
+		return http.build();
+	}
+}
