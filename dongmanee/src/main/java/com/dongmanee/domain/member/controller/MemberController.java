5800520f61a015a4db24f5cdc5cc20196684166d
seongjun-noh <seongjun5657@gmail.com>
2023-11-15 03:26:00 +0900

    feat: Member Controller 추가

---
+++ dongmanee/src/main/java/com/dongmanee/domain/member/controller/MemberController.java
@@ -0,0 +1,28 @@
+package com.dongmanee.domain.member.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dongmanee.domain.member.domain.Member;
+import com.dongmanee.domain.member.dto.request.RequestSignup;
+import com.dongmanee.domain.member.mapper.MemberMapper;
+import com.dongmanee.domain.member.service.MemberService;
+import com.dongmanee.global.utils.ApiResponse;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+	private final MemberService memberService;
+	private final MemberMapper memberMapper;
+
+	@PostMapping("/signup")
+	public ApiResponse UserSignUp(@Valid @RequestBody RequestSignup request) {
+		Member newMember = memberMapper.toEntity(request);
+		memberService.signup(newMember);
+		return ApiResponse.success("회원가입 성공");
+	}
+}
