11c6752f2faf0c4fe1be82d59ae0cd7c8e58cae2
seongjun-noh <seongjun5657@gmail.com>
2023-11-15 02:05:45 +0900

    feat: 공통 응답 처리를 위한 ApiResponse 클래스 추가

---
+++ dongmanee/src/main/java/com/dongmanee/utils/ApiResponse.java
@@ -0,0 +1,30 @@
+package com.dongmanee.utils;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class ApiResponse<T> {
+	private int status;
+	private String message;
+	private T data;
+
+	public static ApiResponse success(String message) {
+		return new ApiResponse<>(HttpStatus.OK.value(), message, null);
+	}
+
+	public static <T> ApiResponse<T> success(T data, String message) {
+		return new ApiResponse<>(HttpStatus.OK.value(), message, data);
+	}
+
+	public static ResponseEntity<ApiResponse> error(HttpStatus errorCode, String message) {
+		return ResponseEntity.status(errorCode).body(new ApiResponse<>(errorCode.value(), message, null));
+	}
+}
