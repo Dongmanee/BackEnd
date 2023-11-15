c250bbb22dcca41cf580875510794fe4899ba452
seongjun_noh <seongjun5657@gmail.com>
2023-11-15 21:13:51 +0900

    feat: university entity에 getter setter 생성자 빌더 추가

--- dongmanee/src/main/java/com/dongmanee/domain/university/domain/University.java
+++ dongmanee/src/main/java/com/dongmanee/domain/university/domain/University.java
@@ -7,9 +7,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "university")
 public class University extends BaseEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
