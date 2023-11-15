af709aeb7018f92116c7dcedc14b297c1136e487
tadaHP <gusvy0220@gmail.com>
2023-11-15 17:14:09 +0900

    feat: 클럽 서비스를 위한 Entity 생성

---
+++ dongmanee/src/main/java/com/dongmanee/domain/club/domain/ClubCategory.java
@@ -0,0 +1,25 @@
+package com.dongmanee.domain.club.domain;
+
+import com.dongmanee.global.entity.BaseEntity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "club_categorys")
+public class ClubCategory extends BaseEntity {
+
+	@Id
+	private Long id;
+
+	private String name;
+}
