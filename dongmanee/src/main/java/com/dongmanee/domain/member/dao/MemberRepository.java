3b18d09bdb434c947d186560df31b2f1cc2a4c90
tadaHP <gusvy0220@gmail.com>
2023-11-17 00:05:15 +0900

    fix: Memeber객체 Optional로 감싸기

--- dongmanee/src/main/java/com/dongmanee/domain/member/dao/MemberRepository.java
+++ dongmanee/src/main/java/com/dongmanee/domain/member/dao/MemberRepository.java
@@ -1,10 +1,13 @@
 package com.dongmanee.domain.member.dao;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dongmanee.domain.member.domain.Member;
+import com.dongmanee.global.security.login.service.port.LoginMemberRepository;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long>, LoginMemberRepository {
 	boolean existsByLoginId(String loginId);
 
 	boolean existsByStudentId(String studentId);
@@ -13,7 +16,5 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
 	boolean existsByPhone(String phone);
 
-	Member findByLoginId(String loginId);
+	Optional<Member> findByLoginId(String loginId);
 }
-
-
