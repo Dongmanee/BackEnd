44f13c09a4012fa29f0b8ee8564145699f19d016
tadaHP <gusvy0220@gmail.com>
2023-11-16 11:15:19 +0900

    feat: SecurityConfig 에서 session 비활성화 및 람다식으로 변환

--- dongmanee/src/main/java/com/dongmanee/global/security/config/SecurityConfig.java
+++ dongmanee/src/main/java/com/dongmanee/global/security/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,17 +18,17 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
-			.httpBasic(httpSecurityHttpBasicConfigurer -> httpSecurityHttpBasicConfigurer.disable()) // spring security 기본 인증 해제
+			.httpBasic(AbstractHttpConfigurer::disable) // spring security 기본 인증 해제
 			.csrf(AbstractHttpConfigurer::disable) // CSRF 공격 방지 기능 해제
 			.cors(Customizer.withDefaults()) // CORS 설
 			.headers((headerConfig) ->    // h2-console 표시를 위해
-				headerConfig.frameOptions(frameOptionsConfig ->
-					frameOptionsConfig.disable()
-				)
+				headerConfig.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
 			)
 			.authorizeHttpRequests(authorizedRequests -> // HTTP 요청에 대한 인가 설정
 				authorizedRequests.anyRequest().permitAll() // 모든 요청에 대해서 인증 없이 허용
-			);
+			)
+			.sessionManagement(
+				(session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // session 비활성화
 
 		return http.build();
 	}
