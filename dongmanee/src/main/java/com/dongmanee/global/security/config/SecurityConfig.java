321353a0f75a1edb593b31a892ec11ef847e4969
tadaHP <gusvy0220@gmail.com>
2023-11-17 00:01:55 +0900

    feat: jwt인증을 위한 Security 설정 1. formLogin 인증 해재 2. 세션 비활성화 3. UsernamePasswordAuthenticationFilter 앞에 Jwt필터 추가 4. 예외 핸들러 추가   1. DeniedHandler   2. EntryPoint

--- dongmanee/src/main/java/com/dongmanee/global/security/config/SecurityConfig.java
+++ dongmanee/src/main/java/com/dongmanee/global/security/config/SecurityConfig.java
@@ -11,24 +11,45 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.dongmanee.global.security.login.entrypoint.JwtAuthenticationEntryPoint;
+import com.dongmanee.global.security.login.filter.JwtAuthenticationFilter;
+import com.dongmanee.global.security.login.handler.JwtAccessDeniedHandler;
+import com.dongmanee.global.security.login.provider.JwtProvider;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+	private final JwtProvider jwtProvider;
+
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
 			.httpBasic(AbstractHttpConfigurer::disable) // spring security 기본 인증 해제
+			.formLogin(AbstractHttpConfigurer::disable)
 			.csrf(AbstractHttpConfigurer::disable) // CSRF 공격 방지 기능 해제
-			.cors(Customizer.withDefaults()) // CORS 설
+			.cors(Customizer.withDefaults()) // CORS 설정
+
 			.headers((headerConfig) ->    // h2-console 표시를 위해
 				headerConfig.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
 			)
-			.authorizeHttpRequests(authorizedRequests -> // HTTP 요청에 대한 인가 설정
-				authorizedRequests.anyRequest().permitAll() // 모든 요청에 대해서 인증 없이 허용
-			)
+			.authorizeHttpRequests(authorizedRequests -> { // HTTP 요청에 대한 인가 설정
+				authorizedRequests.anyRequest().permitAll(); // 모든 요청에 대해서 인증 없이 허용
+			})
 			.sessionManagement(
-				(session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // session 비활성화
+				(session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // session 비활성
+
+			.addFilterBefore(new JwtAuthenticationFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class)
+
+			.exceptionHandling((exception) -> {
+				exception.accessDeniedHandler(new JwtAccessDeniedHandler());
+				exception.authenticationEntryPoint(new JwtAuthenticationEntryPoint());
+			});
 
 		return http.build();
 	}
