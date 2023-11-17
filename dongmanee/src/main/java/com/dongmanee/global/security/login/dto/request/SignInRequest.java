54e542759a3132442841a724910ef2420f8a9714
tadaHP <gusvy0220@gmail.com>
2023-11-17 12:20:33 +0900

    feat: 로그인 요청시 @Pattern 검증도 사용

--- dongmanee/src/main/java/com/dongmanee/global/security/login/dto/request/SignInRequest.java
+++ dongmanee/src/main/java/com/dongmanee/global/security/login/dto/request/SignInRequest.java
@@ -1,6 +1,7 @@
 package com.dongmanee.global.security.login.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 import lombok.Data;
 
@@ -8,7 +9,11 @@ import lombok.Data;
 @Data
 public class SignInRequest {
 	@NotBlank(message = "로그인 id를 입력해 주세요")
+	@Pattern(regexp = "^[a-zA-Z0-9]{6,12}$", message = "아이디는 6~12자 영문 대 소문자, 숫자만 사용 가능합니다.")
 	private String loginId;
+
 	@NotBlank(message = "비밀번호를 입력해주세요")
+	@Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}",
+		message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
 	private String password;
 }
