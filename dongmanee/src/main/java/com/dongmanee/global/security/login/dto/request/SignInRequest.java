7d9c715656adbc9f2afc105a8b979538f4216941
tadaHP <gusvy0220@gmail.com>
2023-11-17 11:22:31 +0900

    fix: 로그인 요청시 @NotBlank 어노테이션을 추가하여 @Valid 사용

--- dongmanee/src/main/java/com/dongmanee/global/security/login/dto/request/SignInRequest.java
+++ dongmanee/src/main/java/com/dongmanee/global/security/login/dto/request/SignInRequest.java
@@ -1,11 +1,14 @@
 package com.dongmanee.global.security.login.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Data;
 
 @Builder
 @Data
 public class SignInRequest {
+	@NotBlank(message = "로그인 id를 입력해 주세요")
 	private String loginId;
+	@NotBlank(message = "비밀번호를 입력해주세요")
 	private String password;
 }
