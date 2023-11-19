d3bba1ad065a12480124c69753651a6efd036522
seongjun-noh <seongjun5657@gmail.com>
2023-11-19 23:13:55 +0900

    feat: Getter 추가

--- dongmanee/src/main/java/com/dongmanee/domain/security/dto/request/SignInRequest.java
+++ dongmanee/src/main/java/com/dongmanee/domain/security/dto/request/SignInRequest.java
@@ -4,11 +4,13 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Builder
 @Setter
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class SignInRequest {
