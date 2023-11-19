a509b0118322c713236f276eec61a800a6d0b52c
seongjun-noh <seongjun5657@gmail.com>
2023-11-19 22:25:31 +0900

    feat: Local, OAuth 로그인에 관한 설정 추가

--- dongmanee/src/main/java/com/dongmanee/domain/security/config/SecurityConfig.java
+++ dongmanee/src/main/java/com/dongmanee/domain/security/config/SecurityConfig.java
@@ -2,7 +2,9 @@ package com.dongmanee.domain.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -12,11 +14,15 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.dongmanee.domain.security.entrypoint.CustomAuthenticationEntryPoint;
+import com.dongmanee.domain.security.filter.CustomAuthenticationFilter;
 import com.dongmanee.domain.security.filter.JwtAuthenticationFilter;
 import com.dongmanee.domain.security.handler.CustomAccessDeniedHandler;
+import com.dongmanee.domain.security.handler.CustomAuthenticationSuccessHandler;
 import com.dongmanee.domain.security.provider.JwtProvider;
+import com.dongmanee.domain.security.service.AuthService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -26,30 +32,69 @@ import lombok.RequiredArgsConstructor;
 public class SecurityConfig {
 
 	private final JwtProvider jwtProvider;
+	private final AuthService authService;
+	private final CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		AuthenticationManager authenticationManager = getAuthenticationManager(http);
+
 		http.httpBasic(AbstractHttpConfigurer::disable) // spring security 기본 인증 해제
-			.formLogin(AbstractHttpConfigurer::disable)
+			// .formLogin(AbstractHttpConfigurer::disable)
 			.csrf(AbstractHttpConfigurer::disable) // CSRF 공격 방지 기능 해제
 			.cors(Customizer.withDefaults()) // CORS 설정
-
 			.headers((headerConfig) ->    // h2-console 표시를 위해
 				headerConfig.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
-			.authorizeHttpRequests(authorizedRequests -> { // HTTP 요청에 대한 인가 설정
-				authorizedRequests.anyRequest().permitAll(); // 모든 요청에 대해서 인증 없이 허용
-			})
 			.sessionManagement(
 				(session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // session 비활성
+			// HTTP 요청에 대한 인가 설정
+			.authorizeHttpRequests(
+				authorizedRequests -> authorizedRequests
+					.requestMatchers(new AntPathRequestMatcher("/login"))
+					.permitAll() // 모든 요청에 대해서 인증 없이 허용
+					.requestMatchers(new AntPathRequestMatcher("/oauth2/**"))
+					.permitAll() // 모든 요청에 대해서 인증 없이 허용
+					.requestMatchers(new AntPathRequestMatcher("/signup"))
+					.permitAll()
+					.anyRequest().permitAll()
+			)
 
+			// 로컬 로그인
+			.authenticationManager(authenticationManager)
+			.addFilter(getCustomAuthenticationFilter(authenticationManager))
+			// OAuth2 로그인
+			.oauth2Login((oauth2Login) -> oauth2Login.userInfoEndpoint(
+					(userInfoEndpointConfig -> userInfoEndpointConfig.userService(authService)))
+				.successHandler(customAuthenticationSuccessHandler))
+
+			// JWT 검증 및 인증
 			.addFilterBefore(new JwtAuthenticationFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class)
-
 			.exceptionHandling((exception) -> {
 				exception.accessDeniedHandler(new CustomAccessDeniedHandler());
 				exception.authenticationEntryPoint(new CustomAuthenticationEntryPoint());
 			});
 
 		return http.build();
+	}
+
+	private AuthenticationManager getAuthenticationManager(HttpSecurity http) throws Exception {
+		// 인증 매니저를 구성하는 빌더 클래스
+		AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(
+			AuthenticationManagerBuilder.class);
+
+		// userDetailService: 사용자 인증 정보를 검색할 때 사용하는 서비스 (userService)
+		// passwordEncoder: 패스워드 인코딩을 위해 사용
+		authenticationManagerBuilder.userDetailsService(authService).passwordEncoder(passwordEncoder());
+		return authenticationManagerBuilder.build();
+	}
+
+	private CustomAuthenticationFilter getCustomAuthenticationFilter(AuthenticationManager authenticationManager) {
+		CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManager);
+		// 로그인 성공 시
+		customAuthenticationFilter.setAuthenticationSuccessHandler(customAuthenticationSuccessHandler);
+		// 로그인 실패 시
+		// customAuthenticationFilter.setAuthenticationFailureHandler(customAuthenticationSuccessHandler);
+		return customAuthenticationFilter;
 	}
 
 	@Bean
