4efc7412bd8a47fbe2a58b590651ab33d66056e1
seongjun-noh <seongjun5657@gmail.com>
2023-11-24 13:05:02 +0900

    refactor: 인증 객체 생성 시 memberId, email값을 함께 성공 핸들러에 넘기도록 수정

--- dongmanee/src/main/java/com/dongmanee/domain/security/dto/OAuthAttributes.java
+++ dongmanee/src/main/java/com/dongmanee/domain/security/dto/OAuthAttributes.java
@@ -1,45 +1,46 @@
 package com.dongmanee.domain.security.dto;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public class OAuthAttributes {
 	private Map<String, Object> attributes;
 	private String nameAttributeKey;
-	private String email;
 
 	@Builder
 	public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String email) {
 		this.attributes = attributes;
 		this.nameAttributeKey = nameAttributeKey;
-		this.email = email;
 	}
 
 	public static OAuthAttributes of(String registrationId, String userNameAttributeName,
 		Map<String, Object> attributes) {
-		// if("kakao".equals(registrationId)){
 		return ofKakao("id", attributes);
-		// }
-		// // naver
-		// if("naver".equals(registrationId)){
-		// 	return ofNaver("id", attributes);
-		// }
-		// // google
-		// return ofGoogle(userNameAttributeName, attributes);
 	}
 
 	private static OAuthAttributes ofKakao(String userNameAttributeName, Map<String, Object> attributes) {
 		// kakao는 kakao_account에 유저정보가 있다. (email)
 		Map<String, Object> kakaoAccount = (Map<String, Object>)attributes.get("kakao_account");
+		Map<String, Object> modifiableAttributes = addExtraAttributes(attributes, kakaoAccount.get("email"));
 		// kakao_account안에 또 profile이라는 JSON객체가 있다. (nickname, profile_image)
 		// Map<String, Object> kakaoProfile = (Map<String, Object>)kakaoAccount.get("profile");
 		return OAuthAttributes.builder()
 			.email((String)kakaoAccount.get("email"))
-			.attributes(attributes)
+			.attributes(modifiableAttributes)
 			.nameAttributeKey(userNameAttributeName)
 			.build();
+	}
+
+	private static Map<String, Object> addExtraAttributes(Map<String, Object> unmodifiableAttributes, Object email) {
+		Map<String, Object> modifiableAttributes = new HashMap<>(unmodifiableAttributes);
+		modifiableAttributes.put("email", email);
+
+		return modifiableAttributes;
 	}
 }
