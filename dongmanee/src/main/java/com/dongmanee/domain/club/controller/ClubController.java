900acf5248d98f256a67cf23171868786c7bcb94
tadaHP <gusvy0220@gmail.com>
2023-11-24 16:38:50 +0900

    feat: Club 생성, Club 정보 수정 기능 생서 1. Controller 2개로 분리 2. 요청을 위한 RequestDto 생성 3. 예외 처리를 위한 Exception 생성 4. ClubSns로 변환을 위한 Mapper 생성 5. 클럽 생성과 정보 수정을 위한 ClubService, ClubInfoUpdateService 생성 - 구현체는 하나

---
+++ dongmanee/src/main/java/com/dongmanee/domain/club/controller/ClubController.java
@@ -0,0 +1,39 @@
+package com.dongmanee.domain.club.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dongmanee.domain.club.domain.Club;
+import com.dongmanee.domain.club.dto.request.RequestCreateClub;
+import com.dongmanee.domain.club.mapper.ClubMapper;
+import com.dongmanee.domain.club.service.ClubService;
+import com.dongmanee.domain.member.domain.Member;
+import com.dongmanee.domain.member.service.MemberService;
+import com.dongmanee.global.utils.ApiResponse;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class ClubController {
+	private final ClubMapper clubMapper;
+	private final ClubService clubService;
+	private final MemberService memberService;
+
+	@PostMapping("/club")
+	public ApiResponse<?> createClub(@Valid @RequestBody RequestCreateClub createClub,
+		@AuthenticationPrincipal UserDetails userDetails) {
+		Member requestMember = memberService.getMemberFromUserId(Long.parseLong(userDetails.getUsername()));
+		Club club = clubMapper.toEntity(createClub);
+		clubService.createClub(club, requestMember);
+		return ApiResponse.success("클럽이 생성되었습니다.");
+	}
+
+	// TODO 1. 클럽 가입 요청 기능 추가
+}
---
+++ dongmanee/src/main/java/com/dongmanee/domain/club/dto/request/RequestSns.java
@@ -0,0 +1,17 @@
+package com.dongmanee.domain.club.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RequestSns {
+	private String title;
+	private String url;
+}
