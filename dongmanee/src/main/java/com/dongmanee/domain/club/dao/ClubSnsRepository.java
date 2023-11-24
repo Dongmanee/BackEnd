0926b71d8d0bd16a8bca8310ee478fd684199398
tadaHP <gusvy0220@gmail.com>
2023-11-24 16:31:21 +0900

    feat: 기능 생성을 위한 레포지토리 생성

---
+++ dongmanee/src/main/java/com/dongmanee/domain/club/dao/ClubSnsRepository.java
@@ -0,0 +1,8 @@
+package com.dongmanee.domain.club.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dongmanee.domain.club.domain.ClubSns;
+
+public interface ClubSnsRepository extends JpaRepository<ClubSns, Long> {
+}
