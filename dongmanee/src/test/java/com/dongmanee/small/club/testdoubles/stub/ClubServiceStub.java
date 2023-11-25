8a4355ef7736f353f45fb8d6edfd1c9af2f2f385
tadaHP <gusvy0220@gmail.com>
2023-11-25 20:06:53 +0900

    test: 테스트를 위한 stub 생성

---
+++ dongmanee/src/test/java/com/dongmanee/small/club/testdoubles/stub/ClubServiceStub.java
@@ -0,0 +1,12 @@
+package com.dongmanee.small.club.testdoubles.stub;
+
+import com.dongmanee.domain.club.domain.Club;
+import com.dongmanee.domain.club.service.ClubService;
+import com.dongmanee.domain.member.domain.Member;
+
+public class ClubServiceStub implements ClubService {
+	@Override
+	public void createClub(Club club, Member member) {
+
+	}
+}
