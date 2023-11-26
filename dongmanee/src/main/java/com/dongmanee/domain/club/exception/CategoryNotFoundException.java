aa715538829beacf4a474c5a305059447852a172
tadaHP <gusvy0220@gmail.com>
2023-11-26 21:29:19 +0900

    refactor: 카테고리가 발견되지 않았을 때 InternalServerError -> NotFound 로 변경

--- dongmanee/src/main/java/com/dongmanee/domain/club/exception/CategoryNotFoundException.java
+++ dongmanee/src/main/java/com/dongmanee/domain/club/exception/CategoryNotFoundException.java
@@ -6,7 +6,7 @@ import com.dongmanee.global.error.exception.CustomException;
 
 public class CategoryNotFoundException extends CustomException {
 
-	private final HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+	private final HttpStatus httpStatus = HttpStatus.NOT_FOUND;
 
 	public CategoryNotFoundException() {
 		super("내부 오류로 카테고리가 잘못 설정되었습니다.");
