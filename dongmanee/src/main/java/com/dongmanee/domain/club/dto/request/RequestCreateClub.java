07a49f0cb90c8ecc1ab06f2883f10e6479d3adf0
tadaHP <gusvy0220@gmail.com>
2023-11-30 23:14:47 +0900

    refactor: Request NotNull -> NotEmpty로 변경

--- dongmanee/src/main/java/com/dongmanee/domain/club/dto/request/RequestCreateClub.java
+++ dongmanee/src/main/java/com/dongmanee/domain/club/dto/request/RequestCreateClub.java
@@ -1,5 +1,6 @@
 package com.dongmanee.domain.club.dto.request;
 
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,9 +14,9 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RequestCreateClub {
-	@NotNull(message = "카테고리 id가 입력되지 않았습니다")
+	@NotEmpty(message = "카테고리 id가 입력되지 않았습니다")
 	private Long categoryId;
-	@NotNull(message = "동아리의 이름을 지정하여하 합니다.")
+	@NotEmpty(message = "동아리의 이름을 지정하여하 합니다.")
 	private String name;
 	private String description;
 	private String clubMainImageUrl;
