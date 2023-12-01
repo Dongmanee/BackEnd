4729d6688000998d3a2f281e2de875a30bc6fe6f
tadaHP <gusvy0220@gmail.com>
2023-12-02 02:53:15 +0900

    docs: RequestDto 에 Swagger 추가

--- dongmanee/src/main/java/com/dongmanee/domain/club/dto/request/RequestSns.java
+++ dongmanee/src/main/java/com/dongmanee/domain/club/dto/request/RequestSns.java
@@ -1,5 +1,6 @@
 package com.dongmanee.domain.club.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,8 +14,10 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RequestSns {
+	@Schema(description = "생성, 수정할 클럽의 Sns 제목", example = "String type Club Sns Title")
 	@NotEmpty
 	private String title;
+	@Schema(description = "생성, 수정할 클럽의 Sns Url", example = "String type Club Sns Url")
 	@NotEmpty
 	private String url;
 }
