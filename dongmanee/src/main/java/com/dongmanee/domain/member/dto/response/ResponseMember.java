d355ba7852a9a4c7152b760503e4160341bef575
seongjun-noh <seongjun5657@gmail.com>
2023-12-04 03:48:27 +0900

    feat: Api 문서화를 위한 Schema 정보 추가

--- dongmanee/src/main/java/com/dongmanee/domain/member/dto/response/ResponseMember.java
+++ dongmanee/src/main/java/com/dongmanee/domain/member/dto/response/ResponseMember.java
@@ -2,6 +2,7 @@ package com.dongmanee.domain.member.dto.response;
 
 import java.io.Serializable;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Value;
 
@@ -10,14 +11,18 @@ import lombok.Value;
  */
 @Value
 public class ResponseMember implements Serializable {
-	@NotNull
+	@Schema(description = "사용자의 학교")
+	@NotNull()
 	String university;
 
+	@Schema(description = "사용자의 학과")
 	@NotNull
 	String department;
 
+	@Schema(description = "사용자의 이름")
 	@NotNull
 	String name;
 
+	@Schema(description = "사용자의 프로필 이미지 Url")
 	String profileImageUrl;
 }
