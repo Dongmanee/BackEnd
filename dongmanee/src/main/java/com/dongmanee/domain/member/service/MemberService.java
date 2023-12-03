4891f6a5eb838b4e8486af99a8d18d67b55a9bb5
seongjun-noh <seongjun5657@gmail.com>
2023-12-04 02:42:33 +0900

    chore: getMemberFromUserId → findById로 변경

--- dongmanee/src/main/java/com/dongmanee/domain/member/service/MemberService.java
+++ dongmanee/src/main/java/com/dongmanee/domain/member/service/MemberService.java
@@ -3,5 +3,5 @@ package com.dongmanee.domain.member.service;
 import com.dongmanee.domain.member.domain.Member;
 
 public interface MemberService {
-	Member getMemberFromUserId(Long id);
+	Member findById(Long id);
 }
--- dongmanee/src/main/java/com/dongmanee/domain/member/service/MemberServiceImpl.java
+++ dongmanee/src/main/java/com/dongmanee/domain/member/service/MemberServiceImpl.java
@@ -2,23 +2,21 @@ package com.dongmanee.domain.member.service;
 
 import org.springframework.stereotype.Service;
 
+import com.dongmanee.domain.member.controller.port.MemberControllerMemberService;
 import com.dongmanee.domain.member.dao.MemberRepository;
 import com.dongmanee.domain.member.domain.Member;
-import com.dongmanee.domain.member.enums.Role;
-import com.dongmanee.domain.member.exception.DuplicateEmailException;
-import com.dongmanee.domain.member.exception.DuplicateLoginIdException;
-import com.dongmanee.domain.member.exception.DuplicatePhoneException;
-import com.dongmanee.domain.member.exception.DuplicateStudentIdException;
-import com.dongmanee.domain.member.exception.UserNotFoundException;
+import com.dongmanee.domain.member.exception.MemberNotFoundException;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class MemberServiceImpl implements MemberService {
+public class MemberServiceImpl
+	implements MemberService, MemberControllerMemberService {
 	private final MemberRepository memberRepository;
+
 	@Override
-	public Member getMemberFromUserId(Long id) {
-		return memberRepository.findById(id).orElseThrow(UserNotFoundException::new);
+	public Member findById(Long id) {
+		return memberRepository.findById(id).orElseThrow(MemberNotFoundException::new);
 	}
 }
