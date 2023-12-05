ba5c379ba34196a01a162598b75df508bf24cacd
seongjun-noh <seongjun5657@gmail.com>
2023-12-06 06:17:01 +0900

    fix: NotEmpty → NotBlank로 수정

--- dongmanee/src/main/java/com/dongmanee/domain/member/dto/request/RequestUpdatePassword.java
+++ dongmanee/src/main/java/com/dongmanee/domain/member/dto/request/RequestUpdatePassword.java
@@ -1,7 +1,7 @@
 package com.dongmanee.domain.member.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,13 +17,13 @@ import lombok.Setter;
 public class RequestUpdatePassword {
 	@Schema(description = "사용자의 기존 비밀번호",
 		example = "@@Test1234")
-	@NotEmpty(message = "기존 비밀번호를 입력해주세요")
+	@NotBlank(message = "기존 비밀번호를 입력해주세요")
 	@Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
 	private String existingPassword;
 
 	@Schema(description = "사용자의 새로운 비밀번호",
 		example = "@@Test12345")
-	@NotEmpty(message = "새로운 비밀번호를 입력해주세요")
+	@NotBlank(message = "새로운 비밀번호를 입력해주세요")
 	@Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
 	private String newPassword;
 }
