38e50302caee9c350b6715641dc0d9067e707e15
tadaHP <gusvy0220@gmail.com>
2023-12-08 18:16:27 +0900

    feat: post 관련 엔티티 생성

---
+++ dongmanee/src/main/java/com/dongmanee/domain/post/domain/PostCategory.java
@@ -0,0 +1,37 @@
+package com.dongmanee.domain.post.domain;
+
+import com.dongmanee.domain.club.domain.Club;
+import com.dongmanee.global.entity.BaseEntity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "club_post_category")
+public class PostCategory extends BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "club_id")
+	private Club club;
+
+	private String name;
+	private Boolean isPublic;
+
+}
