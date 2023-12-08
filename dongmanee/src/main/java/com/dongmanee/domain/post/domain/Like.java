c932b09d9c96f9de6e6f9d17232f5810cc2bfbb7
tadaHP <gusvy0220@gmail.com>
2023-12-08 18:16:27 +0900

    feat: post 관련 엔티티 생성

---
+++ dongmanee/src/main/java/com/dongmanee/domain/post/domain/Like.java
@@ -0,0 +1,37 @@
+package com.dongmanee.domain.post.domain;
+
+import com.dongmanee.domain.member.domain.Member;
+import com.dongmanee.global.entity.BaseEntity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "like_post")
+public class Like extends BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(name = "post_id")
+	private Post post;
+
+	@ManyToOne
+	@JoinColumn(name = "member_id")
+	private Member member;
+
+}
---
+++ dongmanee/src/main/java/com/dongmanee/domain/post/domain/Post.java
@@ -0,0 +1,42 @@
+package com.dongmanee.domain.post.domain;
+
+import com.dongmanee.domain.club.domain.Club;
+import com.dongmanee.domain.member.domain.Member;
+import com.dongmanee.global.entity.BaseEntity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "club_post")
+public class Post extends BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "club_id")
+	private Club club;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member member;
+
+	private String title;
+	private String body;
+	private Boolean isDeleted;
+}
