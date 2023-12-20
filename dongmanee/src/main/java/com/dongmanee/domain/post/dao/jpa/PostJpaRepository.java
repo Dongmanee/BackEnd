b8c4cbf3531a2731eaac5ff7ea225832f2aebd19
tadaHP <gusvy0220@gmail.com>
2023-12-20 13:12:04 +0900

    fix: 정렬 asc -> desc로 변환 및 최신 날짜 기준으로 Paging 하게 조건 변화

--- dongmanee/src/main/java/com/dongmanee/domain/post/dao/jpa/PostJpaRepository.java
+++ dongmanee/src/main/java/com/dongmanee/domain/post/dao/jpa/PostJpaRepository.java
@@ -14,19 +14,29 @@ import com.dongmanee.domain.post.domain.Post;
 
 public interface PostJpaRepository extends JpaRepository<Post, Long>, PostRepository {
 
-	@Query("SELECT p FROM Post p LEFT JOIN FETCH p.club LEFT JOIN FETCH p.category LEFT JOIN FETCH p.member WHERE p.club.id = :clubId AND p.createdAt > :cursor ORDER BY p.createdAt ASC")
+	@Query("SELECT p FROM Post p LEFT JOIN FETCH p.club LEFT JOIN FETCH p.category LEFT JOIN FETCH p.member "
+		+ "WHERE p.club.id = :clubId AND p.createdAt < :cursor ORDER BY p.createdAt DESC")
 	List<Post> findEveryPostsAfterCursor(@Param("clubId") Long clubId, LocalDateTime cursor, Pageable pageable);
 
-	@Query("SELECT p FROM Post p WHERE p.category.name = :category AND p.club.id = :clubId AND p.createdAt > :cursor ORDER BY p.createdAt ASC")
+	@Query("SELECT p FROM Post p "
+		+ "LEFT JOIN FETCH p.club LEFT JOIN FETCH p.category LEFT JOIN FETCH p.member "
+		+ "WHERE p.category.name = :category AND p.club.id = :clubId AND p.createdAt > :cursor "
+		+ "ORDER BY p.createdAt DESC")
 	List<Post> findSpecificPostsAfterCursor(@Param("clubId") Long clubId, @Param("category") String category,
 		LocalDateTime cursor,
 		Pageable pageable);
 
-	@Query("SELECT p FROM Post p WHERE p.category.name != '공지사항' AND p.category.name != '문의사항' AND p.club.id = :clubId AND p.createdAt > :cursor ORDER BY p.createdAt ASC")
+	@Query("SELECT p FROM Post p "
+		+ "LEFT JOIN FETCH p.club LEFT JOIN FETCH p.category LEFT JOIN FETCH p.member "
+		+ "WHERE p.category.name != '공지사항' AND p.category.name != '문의사항' AND "
+		+ "p.club.id = :clubId AND p.createdAt > :cursor "
+		+ "ORDER BY p.createdAt DESC")
 	List<Post> findWithoutSpecificPostsAfterCursor(@Param("clubId") Long clubId, LocalDateTime cursor,
 		Pageable pageable);
 
-	@Query("SELECT p FROM Post p WHERE p.club.id = :clubId ORDER BY p.createdAt DESC")
+	@Query("SELECT p FROM Post p "
+		+ "LEFT JOIN FETCH p.club LEFT JOIN FETCH p.category LEFT JOIN FETCH p.member "
+		+ "WHERE p.club.id = :clubId ORDER BY p.createdAt DESC")
 	Page<Post> findPostsByClubId(Long clubId, Pageable pageable);
 
 	default List<Post> findLatestPostByClubId(Long clubId, Pageable pageable) {
