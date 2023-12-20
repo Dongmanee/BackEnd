b514ba397c34c067106f627e4ebed51e37d96f11
tadaHP <gusvy0220@gmail.com>
2023-12-20 12:47:49 +0900

    docs: TODO 추가

--- dongmanee/src/main/java/com/dongmanee/domain/club/dto/response/postsearch/PostSearchResponse.java
+++ dongmanee/src/main/java/com/dongmanee/domain/club/dto/response/postsearch/PostSearchResponse.java
@@ -20,6 +20,7 @@ public class PostSearchResponse {
 	private String postBody;
 	private String postCategoryName;
 	private PostWriter postWriter;
+	// TODO: 아래 Like, Comment 부분은 기능 생성 이후 매퍼에서 기능 구현 변경
 	private Long postLikesNum;
 	private Long postCommentNum;
 	private Boolean isLike;
