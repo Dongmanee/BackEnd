0bcab3c78c2a7d4c1067cbbc07375d75099c56af
seongjun-noh <seongjun5657@gmail.com>
2023-12-23 19:20:24 +0900

    feat: PATCH 요청에서 업데이트할 값과 빈 값을 구분하기 위해 JsonNullable 사용을 위한 설정을 추가함

---
+++ dongmanee/src/main/java/com/dongmanee/global/jackson/mapper/JsonNullableMapper.java
@@ -0,0 +1,17 @@
+package com.dongmanee.global.jackson.mapper;
+
+import org.mapstruct.Mapper;
+import org.openapitools.jackson.nullable.JsonNullable;
+
+@Mapper(componentModel = "spring")
+public interface JsonNullableMapper {
+
+	default <T> JsonNullable<T> wrap(T entity) {
+		return JsonNullable.of(entity);
+	}
+
+	default <T> T unwrap(JsonNullable<T> jsonNullable) {
+		return jsonNullable == null ? null : jsonNullable.orElse(null);
+	}
+
+}
