47d93c72cd28ed5e8ebe8c632e3e6b19b0adc245
tadaHP <gusvy0220@gmail.com>
2024-02-03 23:15:39 +0900

    feat: 클럽 post 생성을 위한 ClubPost 생성 및 연관관계 설정

--- dongmanee/src/main/java/com/dongmanee/domain/club/domain/ClubPost.java
+++ dongmanee/src/main/java/com/dongmanee/domain/club/domain/ClubPost.java
@@ -1,9 +1,5 @@
 package com.dongmanee.domain.club.domain;
 
-import java.util.List;
-
-import com.dongmanee.global.entity.BaseEntity;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -11,7 +7,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -24,22 +19,15 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "club_post_category")
-public class ClubPostCategory extends BaseEntity {
+@Table(name = "club_post")
+public class ClubPost {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-
+	
 	@NotNull
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "club_id")
-	private Club club;
-
-	@NotNull
-	@OneToMany(mappedBy = "clubPostCategory")
-	private List<ClubPost> post;
-
-	private Boolean isPublic;
-	private String name;
+	@JoinColumn(name = "post_category_id")
+	private ClubPostCategory clubPostCategory;
 }
